473a48c1aadbdcc85fbcacf4209bd7a2780f7e19
pmelo-ca <pedromelocf@yahoo.com>
2024-01-10 16:17:25 -0300

    Starting logic

--- pipex_utils/main.c
+++ pipex_utils/main.c
@@ -6,13 +6,21 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/01/10 15:42:40 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/01/10 16:16:58 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+#include "includes/pipex.h"
 
-int main (void)
+int main (int argc, char **argv, char **envp)
 {
-	return;
+	int fd;
+
+	if (argc != 5)
+		return(1);
+	fd = open(argv[1], O_RDWR);
+	if (fd < 0)
+		printf("a\n");
+	printf("%s\n", envp[0]);
+	return 0;
 }
