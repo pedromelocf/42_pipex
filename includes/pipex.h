49302a3cd8dac9b26a846ae8c0bde1d99fabf439
pmelo-ca <pedromelocf@yahoo.com>
2024-01-15 13:13:21 -0300

    Implementing pseudocoude

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:40 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/01/10 15:40:15 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/01/15 11:57:01 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 # define PIPEX_H
 
 # include "../../lib/libft.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+typedef struct s_pipex
+{
+	char	**argv;
+	char	**envp;
+	char	**comands;
+	int		argc;
+	int		*fd;
+}			t_pipex;
 
 #endif
--- pipex_utils/main.c
+++ pipex_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/01/10 16:19:54 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/01/15 13:13:01 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,25 @@
 
 int main (int argc, char **argv, char **envp)
 {
+	t_pipex		s_pipex;
 	int fd;
+	int validation;
 
 	if (argc != 5)
 		return(1);
 	fd = open(argv[1], O_RDWR);
 	if (fd < 0)
-		printf("a\n");
-	printf("%s\n", envp[0]);
+		printf("no such file or directory: %s\n", argv[1]);
+	validation = validate_command(char **argv, char **envp);
+	if (validation == 0) // os dois comandos existem
+		fork;
+	else if (validation == 1) // argv[3] existe e argv[2] não existe
+		tentar executar segundo comando;
+		printar que o primeiro comando é invalido;
+	else if(validation == 2) // argv[2] existe e argv[3] não existe
+		printar que o segundo comando é invalido;
+	else // nenhum dos comandos existem
+		printar que o segundo comando é invalido;
+		printar que o primeiro comando é invalido;
 	return 0;
 }
