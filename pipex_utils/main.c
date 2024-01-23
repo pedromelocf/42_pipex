e1cc97fbf4c4847a4cc4c09ab69d866cbe973d6d
pmelo-ca <pedromelocf@yahoo.com>
2024-01-23 10:40:11 -0300

    Refactoring code

--- pipex_utils/main.c
+++ pipex_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/01/23 10:38:51 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:39:51 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,8 @@ int	main(int argc, char **argv, char **envp)
 	{
 		s_pipex = init_pipex();
 		s_pipex->infile = open(argv[1], O_RDONLY);
-		if (s_pipex->infile < 0) // infile não existe
-		{
-			dup2(s_pipex->outfile, STDOUT_FILENO);
-			execve(s_pipex->pathname[1], argv[3], envp);
-			return (clean_print_return(1, "no such file or directory", argv[1], s_pipex));
-		}
 		s_pipex->outfile = open(argv[4], O_TRUNC | O_CREAT | O_RDWR);
-		if (s_pipex->outfile < 0) // outfile não existe/ não pode ser criado
-			return (clean_print_return(1, "error opening file:", argv[4], s_pipex));
 		pipe(s_pipex->pipe_fd);
-		validate_commands(argv, envp, s_pipex);
 		s_pipex->pid_child1 = fork();
 		if (s_pipex->pid_child1 == 0) //child process cmd1
 		{
