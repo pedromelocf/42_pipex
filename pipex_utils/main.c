d4a8cff37536a8ced6ad18b15b5aea25127b971d
pmelo-ca <pedromelocf@yahoo.com>
2024-01-23 10:39:14 -0300

    Insert validations inside fork

--- pipex_utils/main.c
+++ pipex_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/01/18 18:00:00 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:38:51 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,51 +31,30 @@ int	main(int argc, char **argv, char **envp)
 			return (clean_print_return(1, "error opening file:", argv[4], s_pipex));
 		pipe(s_pipex->pipe_fd);
 		validate_commands(argv, envp, s_pipex);
-		if (s_pipex->validation == 0) // os dois comandos existem
+		s_pipex->pid_child1 = fork();
+		if (s_pipex->pid_child1 == 0) //child process cmd1
 		{
-			s_pipex->pid_child1 = fork();
-			if (s_pipex->pid_child1 == 0) //child process cmd1
-			{
-				close(s_pipex->pipe_fd[0]);
-				dup2(s_pipex->pipe_fd[1], STDOUT_FILENO);
-				close(s_pipex->pipe_fd[1]);
-				dup2(s_pipex->infile, STDIN_FILENO);
-				execve(s_pipex->pathname[0], argv[1], envp);
-				exit(0);
-			}
-			s_pipex->pid_child2 = fork();
-			if (s_pipex->pid_child2 == 0) //child process cmd2
-			{
-				close(s_pipex->pipe_fd[1]);
-				dup2(s_pipex->pipe_fd[0], STDIN_FILENO);
-				close(s_pipex->pipe_fd[0]);
-				dup2(s_pipex->outfile, STDOUT_FILENO);
-				execve(s_pipex->pathname[1], argv[4], envp);
-				exit(0);
-			}
+			close(s_pipex->pipe_fd[0]);
+			dup2(s_pipex->pipe_fd[1], STDOUT_FILENO);
+			close(s_pipex->pipe_fd[1]);
+			dup2(s_pipex->infile, STDIN_FILENO);
+			execve(s_pipex->pathname[0], argv[1], envp);
+			exit(0);
 		}
-		else if (s_pipex->validation == 1) // argv[3] é um comando valido e argv[2] não é um comando valido
+		s_pipex->pid_child2 = fork();
+		if (s_pipex->pid_child2 == 0) //child process cmd2
 		{
+			close(s_pipex->pipe_fd[1]);
+			dup2(s_pipex->pipe_fd[0], STDIN_FILENO);
+			close(s_pipex->pipe_fd[0]);
 			dup2(s_pipex->outfile, STDOUT_FILENO);
-			execve(s_pipex->pathname[1], argv[3], envp);
-			free(s_pipex);
-			printf("invalid command: %s\n", argv[2]); //printar que o primeiro comando é invalido;
-		}
-		else if (s_pipex->validation == 2)// argv[2] é um comando valido e argv[3] não é um comando valido
-		{
-			free(s_pipex);
-			printf("invalid command: %s\n", argv[3]);//printar que o segundo comando é invalido;
-		}
-		else // nenhum dos comandos são validos
-		{
-			free(s_pipex);
-			printf("invalid command: %s\n", argv[3]); //printar que o segundo comando é invalido;
-			printf("invalid command: %s\n", argv[2]); //printar que o primeiro comando é invalido;
+			execve(s_pipex->pathname[1], argv[4], envp);
+			exit(0);
 		}
 		waitpid(s_pipex->pid_child2,&s_pipex->status, 0);
-		free(s_pipex);
 		close(s_pipex->infile);
 		close(s_pipex->outfile);
+		free(s_pipex);
 	}
 	else
 		printf("Bad arguments.");
