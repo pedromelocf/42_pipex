7d8b468e6e3e55f717d01e1a8681d2352c440609
Pedro Melo <pedromelocf@yahoo.com>
2024-02-28 20:40:38 -0300

    Closing fds that still open in some cases

--- pipex_utils/main.c
+++ pipex_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 16:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/28 20:21:17 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/28 20:39:37 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,17 +60,19 @@ void	child_process1(t_pipex *s_pipex)
 	s_pipex->pid_child1 = fork();
 	if (s_pipex->pid_child1 == 0)
 	{
+		close(s_pipex->pipe_fd[0]);
 		s_pipex->infile = open(s_pipex->argv[1], O_RDONLY);
 		if (s_pipex->infile == -1)
 		{
+			close(s_pipex->pipe_fd[1]);
 			if (errno == EACCES)
 				error_handler(1, s_pipex, NULL, s_pipex->argv[1]);
 			error_handler(2, s_pipex, NULL, s_pipex->argv[1]);
 		}
-		close(s_pipex->pipe_fd[0]);
 		dup2(s_pipex->pipe_fd[1], STDOUT_FILENO);
 		dup2(s_pipex->infile, STDIN_FILENO);
 		close(s_pipex->pipe_fd[1]);
+		close(s_pipex->infile);
 		execute(s_pipex, s_pipex->argv[2]);
 		exit(EXIT_SUCCESS);
 	}
@@ -81,14 +83,18 @@ void	child_process2(t_pipex *s_pipex)
 	s_pipex->pid_child2 = fork();
 	if (s_pipex->pid_child2 == 0)
 	{
+		close(s_pipex->pipe_fd[1]);
 		s_pipex->outfile = open(s_pipex->argv[4], O_CREAT | O_WRONLY | O_TRUNC,
 				S_IRWXU | S_IRWXG | S_IRWXO);
 		if (s_pipex->outfile == -1)
+		{
+			close(s_pipex->pipe_fd[0]);
 			error_handler(1, s_pipex, NULL, s_pipex->argv[4]);
-		close(s_pipex->pipe_fd[1]);
+		}
 		dup2(s_pipex->pipe_fd[0], STDIN_FILENO);
 		dup2(s_pipex->outfile, STDOUT_FILENO);
 		close(s_pipex->pipe_fd[0]);
+		close(s_pipex->outfile);
 		execute(s_pipex, s_pipex->argv[3]);
 		exit(EXIT_SUCCESS);
 	}
